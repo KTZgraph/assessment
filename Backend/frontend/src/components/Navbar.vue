f45a168940af59c9d878e04dc627a034489836f1
pawlaczyk <pawlaczyk@g.pl>
2019-11-09 23:11:17 +0100

    wstepne ogarniecie styli navbara

--- Backend/frontend/src/components/Navbar.vue
+++ Backend/frontend/src/components/Navbar.vue
@@ -1,27 +1,25 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-light my-navbar">
+    <!-- link do głównej strony aplikacji -->
+    <router-link :to="{ name: 'home' }" class="navbar-brand">System Oceniania</router-link>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <router-link :to="{ name: 'home' }" class="nav-link">Dodaj Dokument</router-link>
+            </li>
+        </ul>
+    </div>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
     </form>
-  </div>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="/accounts/logout/">Wyloguj</a>
+        </li>
+    </ul>
+
 </nav>
 </template>
 
@@ -43,3 +41,4 @@ export default {
   color: #dc3545 !important;
 }
 </style>
+
