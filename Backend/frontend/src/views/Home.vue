a40b155597f3c96066cf36040a9aa9c092ebde12
pawlaczyk <pawlaczyk@g.pl>
2019-11-27 13:21:09 +0100

    glowna strona jak galeria

--- Backend/frontend/src/views/Home.vue
+++ Backend/frontend/src/views/Home.vue
@@ -1,39 +1,46 @@
 <template>
   <div class="home">
-    <div class="container mt-2">
-      <div v-for="doc in documents"
-           :key="doc.id">
-        <p class="mb-0">Dodane przez:
-          <span class="document-author">{{ doc.author }}</span>
-        </p>
-        <span class="document-author">{{ doc.id }}</span>
-      <!--  -->
-      <!--  -->
-        <h2>
-          <router-link
-            :to="{ name: 'document', params: { document_id: doc.id }}"
-            class="document-link"
-          >
-            <b-img class="img-mini" v-bind:src="doc.document_file" fluid-grow alt="Fluid image"></b-img>
-          </router-link>
-        </h2>
-         <a v-bind:href="doc.document_file"><p>Podgląd</p></a>
-        <hr>
+      <!-- galeria wszystkich dokumentow -->
+      <div class="gallery">
+        <div class="gallery-panel" 
+            v-for="doc in documents"
+            :key="doc.id">
+          <p class="mb-0">Autor:
+            <span class="document-author">{{ doc.author }}</span>
+          </p>
+          <span class="document-data">{{ doc.created_at }}</span>
+        <!--  -->
+        <!--  -->
+          <h2>
+            <router-link
+              :to="{ name: 'document', params: { document_id: doc.id }}"
+              class="document-link"
+            >
+              <b-img class="img-mini" v-bind:src="doc.document_file" fluid-grow alt="Fluid image"></b-img>
+            </router-link>
+          </h2>
+          <div class="preview-img-button">
+            <a v-bind:href="doc.document_file">
+              <button type="submit" class="btn btn-sm btn-success">Podgląd pliku</button>
+            </a>
+          </div>
+          <hr>
+        </div>
       </div>
 
+
       <!-- loading more data button section -->
-      <div class="my-4">
+      <div class="my-4 load-more-data">
         <p v-show="loadingDocuments">...loading...</p>
         <button
           v-show="next"
           @click="getDocuments"
-          class="btn btn-sm btn-outline-success"
-          >Load More
+          class="btn btn-sm btn-outline-info"
+          >Więcej dokumentów
         </button>
       </div>
       <!-- end loading more data button section -->
 
-    </div>
   </div>
 </template>
 
@@ -100,6 +107,31 @@ export default {
 .document-link:hover{
   color: #343A40;
   text-decoration: none;
+}
+
+
+.home {
+  width: 100%;
+  height: auto;
+}
+.gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-gap: 1rem;
+    max-width: 80rem;
+    margin: 5rem auto;
+    padding: 0 5rem;
+    text-align:center;
+}
+.gallery-panel img {
+    width: 100%;
+    height: 22vw;
+    object-fit: cover;
+    border-radius: 0.75rem;
+
+}
+.load-more-data{
+  text-align:center;
 }
 
 </style>
